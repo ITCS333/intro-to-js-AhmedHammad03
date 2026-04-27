300e84c6dfc50cec8936dff047dc1adc5219caf5
AhmedHammad03 <202101152@stu.uob.edu.bh>
2026-04-27 15:24:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
  */
 function sum(a, b) {
   // TODO: Implement this function.
+  function sum(a, b) {
+  return a + b;
 }
 
 /**
@@ -13,6 +15,9 @@ function sum(a, b) {
  */
 function reverseString(str) {
   // TODO: Implement this function.
+  function reverseString(str) {
+  return str.split('').reverse().join('');
+
 }
 
 /**
@@ -21,6 +26,10 @@ function reverseString(str) {
  */
 function findLargest(numbers) {
   // TODO: Implement this function.
+  function findLargest(numbers) {
+  if (numbers.length === 0) return null;
+  return Math.max(...numbers);
+
 }
 
 /**
@@ -31,6 +40,11 @@ function findLargest(numbers) {
  */
 function isPalindrome(str) {
   // TODO: Implement this function.
+  function isPalindrome(str) {
+  const cleaned = str.toLowerCase();
+  const reversed = cleaned.split('').reverse().join('');
+  return cleaned === reversed;
+
 }
 
 /**
@@ -39,6 +53,9 @@ function isPalindrome(str) {
  */
 function filterEvenNumbers(numbers) {
   // TODO: Implement this function.
+  function filterEvenNumbers(numbers) {
+  return numbers.filter(num => num % 2 === 0);
+
 }
 
 
